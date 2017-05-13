19f062208eebe79cc12f149d970a5d6a54f0fd62
Mukarram Pasha <mukarram.pasha96@gmail.com>
2017-05-13 10:04:34 +0500

    Add files via upload

---
+++ Automata/FAPlatformGame/FAPlatformGame/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace FAPlatformGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.plat1 = new System.Windows.Forms.PictureBox();
+            this.plat2 = new System.Windows.Forms.PictureBox();
+            this.plat3 = new System.Windows.Forms.PictureBox();
+            this.plat4 = new System.Windows.Forms.PictureBox();
+            this.plat5 = new System.Windows.Forms.PictureBox();
+            this.plat6 = new System.Windows.Forms.PictureBox();
+            this.plat7 = new System.Windows.Forms.PictureBox();
+            this.plat8 = new System.Windows.Forms.PictureBox();
+            this.plat9 = new System.Windows.Forms.PictureBox();
+            this.plat10 = new System.Windows.Forms.PictureBox();
+            this.door = new System.Windows.Forms.PictureBox();
+            this.woody3 = new System.Windows.Forms.PictureBox();
+            this.woody1 = new System.Windows.Forms.PictureBox();
+            this.woody2 = new System.Windows.Forms.PictureBox();
+            this.woody8 = new System.Windows.Forms.PictureBox();
+            this.woody6 = new System.Windows.Forms.PictureBox();
+            this.woody5 = new System.Windows.Forms.PictureBox();
+            this.woody9 = new System.Windows.Forms.PictureBox();
+            this.woody4 = new System.Windows.Forms.PictureBox();
+            this.woody7 = new System.Windows.Forms.PictureBox();
+            this.woody10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.plat1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // plat1
+            // 
+            this.plat1.BackgroundImage = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat1.Location = new System.Drawing.Point(-1, 502);
+            this.plat1.Name = "plat1";
+            this.plat1.Size = new System.Drawing.Size(746, 26);
+            this.plat1.TabIndex = 0;
+            this.plat1.TabStop = false;
+            this.plat1.Click += new System.EventHandler(this.plat1_Click);
+            // 
+            // plat2
+            // 
+            this.plat2.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat2.Location = new System.Drawing.Point(420, 424);
+            this.plat2.Name = "plat2";
+            this.plat2.Size = new System.Drawing.Size(116, 25);
+            this.plat2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat2.TabIndex = 1;
+            this.plat2.TabStop = false;
+            this.plat2.Click += new System.EventHandler(this.plat2_Click);
+            // 
+            // plat3
+            // 
+            this.plat3.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat3.Location = new System.Drawing.Point(34, 401);
+            this.plat3.Name = "plat3";
+            this.plat3.Size = new System.Drawing.Size(116, 25);
+            this.plat3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat3.TabIndex = 2;
+            this.plat3.TabStop = false;
+            this.plat3.Click += new System.EventHandler(this.plat3_Click);
+            // 
+            // plat4
+            // 
+            this.plat4.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat4.Location = new System.Drawing.Point(183, 338);
+            this.plat4.Name = "plat4";
+            this.plat4.Size = new System.Drawing.Size(116, 25);
+            this.plat4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat4.TabIndex = 3;
+            this.plat4.TabStop = false;
+            this.plat4.Click += new System.EventHandler(this.plat4_Click);
+            // 
+            // plat5
+            // 
+            this.plat5.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat5.Location = new System.Drawing.Point(576, 323);
+            this.plat5.Name = "plat5";
+            this.plat5.Size = new System.Drawing.Size(116, 25);
+            this.plat5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat5.TabIndex = 4;
+            this.plat5.TabStop = false;
+            this.plat5.Click += new System.EventHandler(this.plat5_Click_1);
+            // 
+            // plat6
+            // 
+            this.plat6.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat6.Location = new System.Drawing.Point(341, 269);
+            this.plat6.Name = "plat6";
+            this.plat6.Size = new System.Drawing.Size(116, 25);
+            this.plat6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat6.TabIndex = 5;
+            this.plat6.TabStop = false;
+            this.plat6.Click += new System.EventHandler(this.plat6_Click_1);
+            // 
+            // plat7
+            // 
+            this.plat7.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat7.Location = new System.Drawing.Point(67, 243);
+            this.plat7.Name = "plat7";
+            this.plat7.Size = new System.Drawing.Size(116, 25);
+            this.plat7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat7.TabIndex = 6;
+            this.plat7.TabStop = false;
+            this.plat7.Click += new System.EventHandler(this.plat7_Click);
+            // 
+            // plat8
+            // 
+            this.plat8.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat8.Location = new System.Drawing.Point(534, 200);
+            this.plat8.Name = "plat8";
+            this.plat8.Size = new System.Drawing.Size(116, 25);
+            this.plat8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat8.TabIndex = 7;
+            this.plat8.TabStop = false;
+            this.plat8.Click += new System.EventHandler(this.plat8_Click);
+            // 
+            // plat9
+            // 
+            this.plat9.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat9.Location = new System.Drawing.Point(157, 152);
+            this.plat9.Name = "plat9";
+            this.plat9.Size = new System.Drawing.Size(116, 25);
+            this.plat9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat9.TabIndex = 8;
+            this.plat9.TabStop = false;
+            this.plat9.Click += new System.EventHandler(this.plat9_Click);
+            // 
+            // plat10
+            // 
+            this.plat10.Image = global::FAPlatformGame.Properties.Resources.platform;
+            this.plat10.Location = new System.Drawing.Point(358, 123);
+            this.plat10.Name = "plat10";
+            this.plat10.Size = new System.Drawing.Size(116, 25);
+            this.plat10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.plat10.TabIndex = 9;
+            this.plat10.TabStop = false;
+            this.plat10.Click += new System.EventHandler(this.plat10_Click);
+            // 
+            // door
+            // 
+            this.door.Image = global::FAPlatformGame.Properties.Resources.door;
+            this.door.Location = new System.Drawing.Point(386, 12);
+            this.door.Name = "door";
+            this.door.Size = new System.Drawing.Size(61, 115);
+            this.door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.door.TabIndex = 10;
+            this.door.TabStop = false;
+            // 
+            // woody3
+            // 
+            this.woody3.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody3.Location = new System.Drawing.Point(58, 352);
+            this.woody3.Name = "woody3";
+            this.woody3.Size = new System.Drawing.Size(59, 53);
+            this.woody3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody3.TabIndex = 11;
+            this.woody3.TabStop = false;
+            // 
+            // woody1
+            // 
+            this.woody1.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody1.Location = new System.Drawing.Point(228, 454);
+            this.woody1.Name = "woody1";
+            this.woody1.Size = new System.Drawing.Size(59, 53);
+            this.woody1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody1.TabIndex = 12;
+            this.woody1.TabStop = false;
+            // 
+            // woody2
+            // 
+            this.woody2.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody2.Location = new System.Drawing.Point(448, 373);
+            this.woody2.Name = "woody2";
+            this.woody2.Size = new System.Drawing.Size(59, 53);
+            this.woody2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody2.TabIndex = 13;
+            this.woody2.TabStop = false;
+            // 
+            // woody8
+            // 
+            this.woody8.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody8.Location = new System.Drawing.Point(563, 152);
+            this.woody8.Name = "woody8";
+            this.woody8.Size = new System.Drawing.Size(59, 53);
+            this.woody8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody8.TabIndex = 14;
+            this.woody8.TabStop = false;
+            // 
+            // woody6
+            // 
+            this.woody6.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody6.Location = new System.Drawing.Point(368, 215);
+            this.woody6.Name = "woody6";
+            this.woody6.Size = new System.Drawing.Size(59, 53);
+            this.woody6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody6.TabIndex = 14;
+            this.woody6.TabStop = false;
+            // 
+            // woody5
+            // 
+            this.woody5.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody5.Location = new System.Drawing.Point(607, 269);
+            this.woody5.Name = "woody5";
+            this.woody5.Size = new System.Drawing.Size(59, 53);
+            this.woody5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody5.TabIndex = 14;
+            this.woody5.TabStop = false;
+            this.woody5.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // woody9
+            // 
+            this.woody9.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody9.Location = new System.Drawing.Point(183, 104);
+            this.woody9.Name = "woody9";
+            this.woody9.Size = new System.Drawing.Size(59, 53);
+            this.woody9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody9.TabIndex = 15;
+            this.woody9.TabStop = false;
+            // 
+            // woody4
+            // 
+            this.woody4.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody4.Location = new System.Drawing.Point(214, 279);
+            this.woody4.Name = "woody4";
+            this.woody4.Size = new System.Drawing.Size(59, 53);
+            this.woody4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody4.TabIndex = 16;
+            this.woody4.TabStop = false;
+            // 
+            // woody7
+            // 
+            this.woody7.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody7.Location = new System.Drawing.Point(79, 200);
+            this.woody7.Name = "woody7";
+            this.woody7.Size = new System.Drawing.Size(59, 53);
+            this.woody7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody7.TabIndex = 17;
+            this.woody7.TabStop = false;
+            // 
+            // woody10
+            // 
+            this.woody10.Image = global::FAPlatformGame.Properties.Resources._414a1sU36iL__SY355_;
+            this.woody10.Location = new System.Drawing.Point(386, 74);
+            this.woody10.Name = "woody10";
+            this.woody10.Size = new System.Drawing.Size(59, 53);
+            this.woody10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.woody10.TabIndex = 18;
+            this.woody10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FAPlatformGame.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(745, 519);
+            this.Controls.Add(this.woody10);
+            this.Controls.Add(this.woody7);
+            this.Controls.Add(this.woody4);
+            this.Controls.Add(this.woody9);
+            this.Controls.Add(this.woody5);
+            this.Controls.Add(this.woody6);
+            this.Controls.Add(this.woody8);
+            this.Controls.Add(this.woody2);
+            this.Controls.Add(this.woody1);
+            this.Controls.Add(this.woody3);
+            this.Controls.Add(this.door);
+            this.Controls.Add(this.plat10);
+            this.Controls.Add(this.plat9);
+            this.Controls.Add(this.plat8);
+            this.Controls.Add(this.plat7);
+            this.Controls.Add(this.plat6);
+            this.Controls.Add(this.plat5);
+            this.Controls.Add(this.plat4);
+            this.Controls.Add(this.plat3);
+            this.Controls.Add(this.plat2);
+            this.Controls.Add(this.plat1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Platform Game";
+            ((System.ComponentModel.ISupportInitialize)(this.plat1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.plat10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.woody10)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox plat1;
+        private System.Windows.Forms.PictureBox plat2;
+        private System.Windows.Forms.PictureBox plat3;
+        private System.Windows.Forms.PictureBox plat4;
+        private System.Windows.Forms.PictureBox plat5;
+        private System.Windows.Forms.PictureBox plat6;
+        private System.Windows.Forms.PictureBox plat7;
+        private System.Windows.Forms.PictureBox plat8;
+        private System.Windows.Forms.PictureBox plat9;
+        private System.Windows.Forms.PictureBox plat10;
+        private System.Windows.Forms.PictureBox door;
+        private System.Windows.Forms.PictureBox woody3;
+        private System.Windows.Forms.PictureBox woody1;
+        private System.Windows.Forms.PictureBox woody2;
+        private System.Windows.Forms.PictureBox woody8;
+        private System.Windows.Forms.PictureBox woody6;
+        private System.Windows.Forms.PictureBox woody5;
+        private System.Windows.Forms.PictureBox woody9;
+        private System.Windows.Forms.PictureBox woody4;
+        private System.Windows.Forms.PictureBox woody7;
+        private System.Windows.Forms.PictureBox woody10;
+    }
+}
+
---
+++ Automata/FAPlatformGame/FAPlatformGame/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FAPlatformGame
+{
+    public partial class Form1 : Form
+    {
+        int c_state = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            hideAllWoodys();
+            woody1.Visible = true;
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(1, 10));
+        }
+
+        public void hideAllWoodys()
+        {
+            woody1.Visible = false;
+            woody2.Visible = false;
+            woody3.Visible = false;
+            woody4.Visible = false;
+            woody5.Visible = false;
+            woody6.Visible = false;
+            woody7.Visible = false;
+            woody8.Visible = false;
+            woody9.Visible = false;
+            woody10.Visible = false;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plat1_Click(object sender, EventArgs e)
+        {
+            int state = 0;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody1.Visible = true;
+                c_state = 0;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat2_Click(object sender, EventArgs e)
+        {
+            int state = 1;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody2.Visible = true;
+                c_state = 1;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat3_Click(object sender, EventArgs e)
+        {
+            int state = 2;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody3.Visible = true;
+                c_state = 2;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat4_Click(object sender, EventArgs e)
+        {
+            int state = 3;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody4.Visible = true;
+                c_state = 3;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat6_Click_1(object sender, EventArgs e)
+        {
+            int state = 5;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody6.Visible = true;
+                c_state = 5;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat5_Click_1(object sender, EventArgs e)
+        {
+            int state = 4;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody5.Visible = true;
+                c_state = 4;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat7_Click(object sender, EventArgs e)
+        {
+            int state = 6;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody7.Visible = true;
+                c_state = 6;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat8_Click(object sender, EventArgs e)
+        {
+            int state = 7;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody8.Visible = true;
+                c_state = 7;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat9_Click(object sender, EventArgs e)
+        {
+            int state = 8;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody9.Visible = true;
+                c_state = 8;
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+
+        private void plat10_Click(object sender, EventArgs e)
+        {
+            int state = 9;
+            if (dfa.transition(c_state, state))
+            {
+                hideAllWoodys();
+                woody10.Visible = true;
+                c_state = 9;
+                MessageBox.Show("Congratulations Woddy is now home !");
+                Application.Restart();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Move");
+            }
+        }
+    }
+
+    public class dfa
+    {
+
+        static int[,] TransitionTable = new int[,] 
+        {{ 1,1,1,0,0,0,0,0,0,0},
+        { 1,1,0,1,1,0,0,0,0,0},
+        { 1,0,1,1,0,0,0,0,0,0},
+        { 1,1,1,1,0,1,1,0,0,0},
+        { 1,1,0,0,1,1,0,0,0,0},
+        { 0,1,0,1,1,1,0,1,0,0},
+        { 0,0,0,1,0,0,1,0,1,0},
+        { 0,0,0,0,1,1,0,1,0,1},
+        { 0,0,0,0,0,1,1,0,1,1},
+        { 0,0,0,0,0,0,0,0,0,1}};
+
+
+        public static Boolean transition(int curr_state, int next_state)
+        {
+            if (TransitionTable[curr_state, next_state] == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
